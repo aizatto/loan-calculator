520652ca408fccd345caa482278f9069265d77f2
Aizat Faiz <aizat.faiz@gmail.com>
2022-02-05 15:24:14 +0800

    ux: View page include remaining values

--- src/table/ViewButton.tsx
+++ src/table/ViewButton.tsx
@@ -11,14 +11,15 @@ interface Props {
 interface DataSource {
   key: string
   month: number
-  year: number
+  year: string
   interestPaid: number
   principlePaid: number
   totalPrinciplePaid: number
   totalInterestPaid: number
   totalPaid: number
   remainingPrinciple: number
-  lifetimeCostRemaining: number
+  remainingInterest: number
+  remainingLifetimeCost: number
 }
 
 export const ViewButton: React.FC<Props> = (props) => {
@@ -38,24 +39,29 @@ export const ViewButton: React.FC<Props> = (props) => {
   const dataSource: DataSource[] = []
 
   let totalPrinciplePaid = 0
+  let totalInterestPaid = 0
 
-  const rule78 = (months * (months + 1)) / 2
+  const monthlyInterestRate = (months * (months + 1)) / 2
+
   for (let i = 0; i < months; i++) {
-    const interestPaid = ((months - i) / rule78) * record.totalInterest
+    const interestPaid =
+      ((months - i) / monthlyInterestRate) * record.totalInterest
     const principlePaid = record.monthly - interestPaid
     totalPrinciplePaid += principlePaid
+    totalInterestPaid += interestPaid
 
     dataSource.push({
       key: `${record.key}:${i}`,
-      year: Math.floor(i / 12),
+      year: `Y${Math.floor(i / 12) + 1}M${(i % 12) + 1}`,
       month: i + 1,
       interestPaid,
       principlePaid,
+      totalInterestPaid,
       totalPrinciplePaid,
-      totalInterestPaid: 0,
       totalPaid: (i + 1) * record.monthly,
+      remainingInterest: record.totalInterest - totalInterestPaid,
       remainingPrinciple: record.price - totalPrinciplePaid,
-      lifetimeCostRemaining: record.lifetimeCost - (i + 1) * record.monthly,
+      remainingLifetimeCost: record.lifetimeCost - (i + 1) * record.monthly,
     })
   }
 
@@ -87,7 +93,7 @@ export const ViewButton: React.FC<Props> = (props) => {
       },
     },
     {
-      title: 'Total Principle Paid',
+      title: 'Total Interest Paid',
       dataIndex: 'totalPrinciplePaid',
       align: 'right' as AlignType,
       render: (value: number) => {
@@ -95,8 +101,8 @@ export const ViewButton: React.FC<Props> = (props) => {
       },
     },
     {
-      title: 'Remaining Principle',
-      dataIndex: 'remainingPrinciple',
+      title: 'Total Principle Paid',
+      dataIndex: 'totalPrinciplePaid',
       align: 'right' as AlignType,
       render: (value: number) => {
         return Number(value).toLocaleString()
@@ -111,8 +117,24 @@ export const ViewButton: React.FC<Props> = (props) => {
       },
     },
     {
+      title: 'Remaining Interest',
+      dataIndex: 'remainingInterest',
+      align: 'right' as AlignType,
+      render: (value: number) => {
+        return Number(value).toLocaleString()
+      },
+    },
+    {
+      title: 'Remaining Principle',
+      dataIndex: 'remainingPrinciple',
+      align: 'right' as AlignType,
+      render: (value: number) => {
+        return Number(value).toLocaleString()
+      },
+    },
+    {
       title: 'Remaining Lifetime Cost',
-      dataIndex: 'lifetimeCostRemaining',
+      dataIndex: 'remainingLifetimeCost',
       align: 'right' as AlignType,
       render: (value: number) => {
         return Number(value).toLocaleString()
@@ -132,7 +154,7 @@ export const ViewButton: React.FC<Props> = (props) => {
             Cancel
           </Button>,
         ]}
-        width={900}
+        width={1000}
       >
         <div>
           Price:
